a6546c9b556446438dfccd3d02a155d68ac3c906
WilliamTsou818 <cccc1589@gmail.com>
2021-07-20 14:53:47 +0800

    feat: add cart to products page

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,18 +1,26 @@
 const db = require('../models')
 const Product = db.Product
+const Cart = db.Cart
 const PAGE_LIMIT = 3
 const PAGE_OFFSET = 0
 
 const productController = {
   getProducts: (req, res) => {
-    Product.findAndCountAll({
-      raw: true,
-      nest: true,
-      offset: PAGE_OFFSET,
-      limit: PAGE_LIMIT
-    })
-      .then(products => {
-        return res.render('products', { products })
+    return Promise.all([
+      Product.findAndCountAll({
+        raw: true,
+        nest: true,
+        offset: PAGE_OFFSET,
+        limit: PAGE_LIMIT
+      }),
+      Cart.findByPk(req.session.cartId, {
+        include: { model: Product, as: 'items' }
+      })
+    ])
+      .then(([products, cart]) => {
+        cart ? cart = cart.toJSON() : cart = { items: [] }
+        const totalPrice = cart.items.length > 0 ? cart.items.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
+        return res.render('products', { products, cart, totalPrice })
       })
   }
 }

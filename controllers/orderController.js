9eeeb6f4cbb5919cc093f4e32ca8a49ddf458ece
WilliamTsou818 <cccc1589@gmail.com>
2021-07-23 19:21:24 +0800

    feat: add generate tradeInfo function and third-part payment feature

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,12 +1,22 @@
 require('dotenv').config()
 const db = require('../models')
 const nodemailer = require('nodemailer')
+const crypto = require('crypto')
 const Order = db.Order
 const OrderItem = db.OrderItem
 const Cart = db.Cart
 
+const tradeData = {
+  MerchantID: process.env.MERCHANT_ID,
+  HashKey: process.env.HASH_KEY,
+  HashIV: process.env.HASH_IV,
+  Newebpay: 'https://ccore.newebpay.com/MPG/mpg_gateway',
+  ReturnURL: process.env.URL + '/newebpay/callback?from=ReturnURL',
+  NotifyURL: process.env.URL + '/newebpay/callback?from=NotifyURL',
+  ClientBackURL: process.env.URL + '/orders'
+}
+
 async function generateTransporter (order) {
-  console.log(process.env.MAIL_PASSWORD)
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -29,6 +39,73 @@ async function generateTransporter (order) {
       console.log('Email sent: ' + info.response)
     }
   })
+}
+
+function genDataChain (TradeInfo) {
+  const results = []
+  for (const kv of Object.entries(TradeInfo)) {
+    results.push(`${kv[0]}=${kv[1]}`)
+  }
+  return results.join('&')
+}
+
+function create_mpg_aes_encrypt (TradeInfo) {
+  const encrypt = crypto.createCipheriv('aes256', tradeData.HashKey, tradeData.HashIV)
+  const enc = encrypt.update(genDataChain(TradeInfo), 'utf8', 'hex')
+  return enc + encrypt.final('hex')
+}
+
+function create_mpg_sha_encrypt (TradeInfo) {
+  const sha = crypto.createHash('sha256')
+  const plainText = `HashKey=${tradeData.HashKey}&${TradeInfo}&HashIV=${tradeData.HashIV}`
+
+  return sha.update(plainText).digest('hex').toUpperCase()
+}
+
+function getTradeInfo (Amt, Desc, email) {
+  console.log('===== getTradeInfo =====')
+  console.log(Amt, Desc, email)
+  console.log('==========')
+
+  const data = {
+    MerchantID: tradeData.MerchantID, // 商店代號
+    RespondType: 'JSON', // 回傳格式
+    TimeStamp: Date.now(), // 時間戳記
+    Version: 1.6, // 串接程式版本
+    MerchantOrderNo: Date.now(), // 商店訂單編號
+    LoginType: 0, // 智付通會員
+    OrderComment: 'OrderComment', // 商店備註
+    Amt: Amt, // 訂單金額
+    ItemDesc: Desc, // 產品名稱
+    Email: email, // 付款人電子信箱
+    ReturnURL: tradeData.ReturnURL, // 支付完成返回商店網址
+    NotifyURL: tradeData.NotifyURL, // 支付通知網址/每期授權結果通知
+    ClientBackURL: tradeData.ClientBackURL // 支付取消返回商店網址
+  }
+
+  console.log('===== getTradeInfo: data =====')
+  console.log(data)
+
+  const mpg_aes_encrypt = create_mpg_aes_encrypt(data)
+  const mpg_sha_encrypt = create_mpg_sha_encrypt(mpg_aes_encrypt)
+
+  console.log('===== getTradeInfo: mpg_aes_encrypt, mpg_sha_encrypt =====')
+  console.log(mpg_aes_encrypt)
+  console.log(mpg_sha_encrypt)
+
+  const tradeInfo = {
+    MerchantID: tradeData.MerchantID, // 商店代號
+    TradeInfo: mpg_aes_encrypt, // 加密後參數
+    TradeSha: mpg_sha_encrypt,
+    Version: 1.6, // 串接程式版本
+    Newebpay: tradeData.Newebpay,
+    MerchantOrderNo: data.MerchantOrderNo
+  }
+
+  console.log('===== getTradeInfo: tradeInfo =====')
+  console.log(tradeInfo)
+
+  return tradeInfo
 }
 
 const orderController = {
@@ -97,7 +174,8 @@ const orderController = {
     console.log('==========')
 
     return Order.findByPk(req.params.id, {}).then(order => {
-      return res.render('payment', { order: order.toJSON() })
+      const tradeInfo = getTradeInfo(order.amount, '產品', 'cccc1589@gmail.com')
+      return res.render('payment', { order: order.toJSON(), tradeInfo })
     })
   },
   newebpayCallback: (req, res) => {
@@ -105,7 +183,7 @@ const orderController = {
     console.log(req.body)
     console.log('==========')
 
-    return res.redirect('back')
+    return res.redirect('/orders')
   }
 }
 
